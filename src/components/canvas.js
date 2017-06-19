dca1c44ad51d770ae4e7b1b6aca1d991e587d926
debelopumento <imdiye@gmail.com>
2017-06-18 19:07:03 -0700

    created grid canvas

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -2,24 +2,51 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Canvas extends Component {
+	color = event => {
+		const rowId = event.target.id.slice(7, 9);
+		console.log(rowId);
+	};
 	render() {
-		const width = [...Array(this.props.resolution.width).keys()];
-		const height = [...Array(this.props.resolution.height).keys()];
-		const row = Object.keys(width).map(index => {
+		const cellSize = this.props.resolution.cellSize;
+		const rowElement = [...Array(this.props.resolution.width).keys()];
+		const rowNumber = [...Array(this.props.resolution.height).keys()];
+		const row = Object.keys(rowElement).map(index => {
+			const id = `column-${index}`;
 			return (
 				<div
 					key={index}
+					id={id}
 					style={{
 						border: "1px solid black",
-						width: 30,
-						height: 30
+						width: cellSize,
+						height: cellSize,
+						backgroundColor: "#ddd"
 					}}
+					onClick={this.color}
 				/>
 			);
 		});
+		const grid = Object.keys(rowNumber).map(index => {
+			const id = `row-${index}`;
+			return (
+				<div
+					key={index}
+					id={id}
+					style={{
+						width: rowWidth,
+						display: "flex",
+						margin: "auto"
+					}}
+				>
+					{row}
+				</div>
+			);
+		});
+
+		const rowWidth = cellSize * this.props.resolution.width + 10;
 		return (
-			<div style={{ display: "flex" }}>
-				{row}
+			<div style={{ width: rowWidth, margin: "auto" }}>
+				{grid}
 			</div>
 		);
 	}
--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 
-const resolutionReducer = (state = { width: 15, height: 10 }, action) => {
+const resolutionReducer = (
+	state = { width: 45, height: 30, cellSize: 10 },
+	action
+) => {
 	switch (action.type) {
 		case "UPDATE_FIRST_REDUCER": {
 			return action.payload;
